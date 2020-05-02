8fbd69448b1dd1e63480fb4f820c13a4bee68aec
Herman <herman@nygaard.dev>
2020-05-02 03:09:34 +0200

    Add image to README.md

--- README.md
+++ README.md
@@ -1,13 +1,33 @@
 # [Karakterweb](https://karakterweb.no)-CLI
+
 CLI for grade statistics at norwegian universities
 
-## Installation
-Node is needed for this package\
-\
-Try it out without installing anything locally with npx: `npx karakter`\
-Or install it globally on your system: `npm i -g karakter`
+![Demo](assets/new.png)
 
-## API
+## Installation
+
+Try it out without installing locally using npx:\
+`npx karakter`\
+\
+Or install it globally:\
+ `npm i -g karakter`
+
+## Usage
+
+Just type `karakter` in your shell (if installed globally), the program will prompt you for input
+
+The default university is UiO. You can change this default by running with --uni= FLAG/setting option:\
+`karakter --uni=ntnu`
+
+List of universities currently supported:
+
+- uio
+- ntnu
+- uib
+
+Open a pull request/issue or contact if you want an institution added
+
+## Data
+
 Data from [NSD](https://nsd.no)\
 [API license](https://www.data.norge.no/nlod/no/2.0)
-
--- src/index.js
+++ src/index.js
@@ -1,123 +1,131 @@
 #!/usr/bin/env node
-const inquirer = require('inquirer');
-const fetch = require('node-fetch');
-const wunderbar = require('@gribnoysup/wunderbar');
+const inquirer = require("inquirer");
+const fetch = require("node-fetch");
+const wunderbar = require("@gribnoysup/wunderbar");
 
 const printData = (plot) => {
-    const { chart, legend, scale, __raw } = wunderbar(plot, {
-        min: 0,
-        length: 42,
-        randomColorOptions: {
+  const { chart, legend, scale, __raw } = wunderbar(plot, {
+    min: 0,
+    length: 42,
+    randomColorOptions: {},
+    format: "0a",
+  });
 
-        },
-        format: "0a"
-    });
-
-    console.log();
-    console.log(chart);
-    console.log();
-    console.log(scale);
-    console.log();
-    console.log(legend);
+  console.log();
+  console.log(chart);
+  console.log();
+  console.log(scale);
+  console.log();
+  console.log(legend);
 };
 
-const url = "https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData"
-
-const payload = {
-    tabell_id: 308,
-    api_versjon: 1,
-    statuslinje: "N",
-    begrensning: "1000",
-    kodetekst: "N",
-    desimal_separator: ".",
-    groupBy: ["Institusjonskode", "Emnekode", "Karakter", "Årstall"],
-    sortBy: ["Karakter"],
-    filter: [
-        {
-            variabel: "Institusjonskode",
-            selection: { filter: "item", values: ["1110"] }
-        },
-        {
-            variabel: "Emnekode",
-            selection: {
-                filter: "item",
-                values: ["in1010-1"]
-            }
-        },
-        {
-            variabel: "Årstall",
-            selection: {
-                filter: "item",
-                values: [`2019`]
-            }
-        }
-    ]
+function setColor(grade) {
+  // Add colors
+  switch (grade) {
+    case "A":
+      return "#56fed6";
+    case "B":
+      return "#59d7d7";
+    case "C":
+      return "#5baed9";
+    case "D":
+      return "#3380e3";
+    case "E":
+      return "#003fff";
+    case "F":
+      return "#0000FF";
+    case "G":
+      return "#59d7d7";
+    case "H":
+      return "#3380e3";
+  }
 }
 
+const url = "https://api.nsd.no/dbhapitjener/Tabeller/hentJSONTabellData";
+
+const payload = {
+  tabell_id: 308,
+  api_versjon: 1,
+  statuslinje: "N",
+  begrensning: "1000",
+  kodetekst: "N",
+  desimal_separator: ".",
+  groupBy: ["Institusjonskode", "Emnekode", "Karakter", "Årstall"],
+  sortBy: ["Karakter"],
+  filter: [
+    {
+      variabel: "Institusjonskode",
+      selection: { filter: "item", values: ["1110"] },
+    },
+    {
+      variabel: "Emnekode",
+      selection: {
+        filter: "item",
+        values: ["in1010-1"],
+      },
+    },
+    {
+      variabel: "Årstall",
+      selection: {
+        filter: "item",
+        values: [`2019`],
+      },
+    },
+  ],
+};
+
 inquirer
-    .prompt([
-        { type: "input", message: "Emnekode:", name: "emnekode" },
-        { type: "input", message: "Årstall:", name: "year" },
-    ])
-    .then(async ({ emnekode, year }) => {
+  .prompt([
+    { type: "input", message: "Emnekode:", name: "emnekode" },
+    { type: "input", message: "Årstall:", name: "year" },
+  ])
+  .then(async ({ emnekode, year }) => {
+    // Edit payload with the provided input
+    payload.filter[1].selection.values = [`${emnekode.replace(/\s/g, "")}-1`];
+    payload.filter[2].selection.values = [`${year}`];
 
-        // Edit payload with the provided input
-        payload.filter[1].selection.values = [`${emnekode.replace(/\s/g, "")}-1`];
-        payload.filter[2].selection.values = [`${year}`];
+    let data;
+    try {
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: { "Content-Type": "application/json" },
+      });
+      data = await res.json();
+    } catch (e) {
+      console.log("Ingen emner funnet. Er det riktig emnekode for dette året?");
+    }
 
-        let data;
-        try {
-            const res = await fetch(url, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: { "Content-Type": "application/json" }
-            })
-            data = await res.json();
-        } catch (e) {
-            console.log("Ingen emner funnet. Er det riktig emnekode for dette året?")
-        }
-
-        // Get total candidates
-        let total = 0;
-        data.forEach(e => total += Number(e['Antall kandidater totalt']))
-        const plot = data.map(e => {
-
-            // For bestått/ikkebestått
-            if (e['Karakter'] === "G") {
-                e.Karakter = "Bestått"
-            } else if (e['Karakter'] === "H") {
-                e.Karakter = "Ikke bestått"
-            }
-            const candidates = e['Antall kandidater totalt'];
-            // Every grade with percentage 
-            const label = `${e.Karakter} (${Math.round((candidates / total) * 100)}%)`
-            return { value: candidates, label, }
-        })
-
-        // Add colors
-        if (plot[0].label[0] === "A") {
-            plot[0].color = "#56fed6";
-            plot[1].color = "#59d7d7";
-            plot[2].color = "#5baed9";
-            plot[3].color = "#3380e3";
-            plot[4].color = "#003fff";
-            plot[5].color = "#0000FF";
-        } else {
-            // Betått/ikke bestått
-            plot[0].color = "#59d7d7";
-            plot[1].color = "#3380e3"
-        }
-
-        console.log(`\nResultater for ${emnekode.toUpperCase()} året ${year}`)
-        console.log(`Antall kandidater: ${total}`)
-
-        printData(plot);
-        console.log()
-    })
-    .catch(error => {
-        if (error.isTtyError) {
-            // Prompt couldn't be rendered in the current environment
-        } else {
-            // Something else when wrong
-        }
+    // Get total candidates
+    let total = 0;
+    data.forEach((e) => (total += Number(e["Antall kandidater totalt"])));
+    const plot = data.map((e) => {
+      // For bestått/ikkebestått
+      if (e["Karakter"] === "G") {
+        e.Karakter = "Bestått";
+      } else if (e["Karakter"] === "H") {
+        e.Karakter = "Ikke bestått";
+      }
+      const candidates = e["Antall kandidater totalt"];
+      // Every grade with percentage
+      const label = `${e.Karakter} (${Math.round(
+        (candidates / total) * 100
+      )}%)`;
+      const color = setColor(e.Karakter);
+      // Value: number, label = the grade
+      return { value: candidates, label, color };
     });
+
+    console.log(`\nResultater for ${emnekode.toUpperCase()} året ${year}`);
+    console.log(`Antall kandidater: ${total}`);
+
+    printData(plot);
+    console.log();
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else when wrong
+    }
+  });
